6ce77076826f3dcffbcef9869c7a8d3efdcab0eb
Денис Пономарев <deskin196@gmail.com>
2023-03-05 20:44:12 +0300

    Написал код по вводу трех чисел к домашнему заданию номер 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Нахождение максимального из трех чисел");
+
+int[] int_array = new int[3];
+
+for(int i=0; i < int_array.Length; i++)
+{
+    Console.Write($"Введите {i+1}-ое число: ");
+
+    //TryParse то-же что и Parse, но не возникает исключение при строке не содержащей цифры, а возвращает 0, вторым параметром передаем ссылку для возврата результата. 
+    while(  !int.TryParse(Console.ReadLine(), out int_array[i] ))
+    {
+        Console.Write($"Введено некорректное число, повторите попытку ввода {i+1}-го числа:");
+    }
+
+}
