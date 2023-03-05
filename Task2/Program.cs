7b653c3d8fb5a60ffbf40840d2f88d36e2e023c4
Денис Пономарев <deskin196@gmail.com>
2023-03-05 20:54:23 +0300

    Написал код по поиску максимального числа из введенных по домашнему заданию номер 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.WriteLine("Нахождение максимального из трех чисел");
 
+//Достаточно изменить размерность массива, и можно вводить сколько угодно чисел для поиска максимума
 int[] int_array = new int[3];
+int max = 0;
 
 for(int i=0; i < int_array.Length; i++)
 {
@@ -13,3 +15,11 @@ for(int i=0; i < int_array.Length; i++)
     }
 
 }
+
+max = int_array[0];
+
+for(int i=1; i < int_array.Length; i++)
+{
+    if(max < int_array[i]) max = int_array[i];
+}
+Console.WriteLine($"Максимальное число из введенных: {max}");
