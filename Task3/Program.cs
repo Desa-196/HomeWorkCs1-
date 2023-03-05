16353d1449be681dd911bac0d8e65680994d75ae
Денис Пономарев <deskin196@gmail.com>
2023-03-05 22:14:43 +0300

    Написал код для домашней работы 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int int_1 = 0;
+
+Console.WriteLine("Введите число для проверки четности: ");
+
+while(  !int.TryParse(Console.ReadLine(), out int_1 ))
+{
+    Console.Write("Введено некорректное число, повторите попытку ввода: ");
+}
+
+if( int_1 % 2 == 0) Console.WriteLine($"Чило {int_1} является четным.");
+else Console.WriteLine($"Чило {int_1} является не четным.");
