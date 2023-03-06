9263fdb0c633f883cf88a9ef01394aa32a8b644c
Денис Пономарев <deskin196@gmail.com>
2023-03-06 10:15:14 +0300

    Код для для домашней работы 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N = 0;
+Console.WriteLine("Введите число N: ");
+
+while(  !int.TryParse(Console.ReadLine(), out N ))
+{
+    Console.Write("Введено некорректное число, повторите попытку ввода: ");
+}
+
+Console.WriteLine($"Список четных чисел от 1 до {N}:");
+
+//Если N больше нуля
+if(N > 0)
+{
+    for(int i=2; i <= N; i= i+2 )
+    {
+        Console.WriteLine(i);
+    }
+}
+//Если N меньше или равно нулю.
+else
+{
+    for(int i=0; i >= N; i=i-2 )
+    {
+        Console.WriteLine(i);
+    }
+}
